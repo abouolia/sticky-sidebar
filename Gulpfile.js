96cd51276771afda77c519dc6b5a6471951cd053
abouolia <a.bouhuolia@Live.com>
2017-08-29 05:16:36 +0200

    Add more gulp task for new version

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,13 @@
-var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var header = require('gulp-header');
-var concat = require('gulp-concat');
-
-var pkg = require('./package.json');
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const babel = require("gulp-babel");
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+const concat = require('gulp-concat');
+const gutil = require('gulp-util');
+const rollup = require('gulp-rollup');
+const pkg = require('./package.json'); 
 
 const banner = [
   '/**',
@@ -19,10 +20,9 @@ const banner = [
   '',
 ].join('\n');
 
-
 gulp.task("default", ['babel', 'concat', 'uglify']);
 
-gulp.task("babel", function () {
+gulp.task("babel", function(){
   return gulp.src("src/*.js")
     .pipe(sourcemaps.init())
     .pipe(babel())
@@ -30,16 +30,33 @@ gulp.task("babel", function () {
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task('concat', function() {
+gulp.task('concat', ['babel'], function(){
   return gulp.src(['bower_components/raf.js/raf.js', './dist/sticky-sidebar.js'])
     .pipe(concat('sticky-sidebar.js'))
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('uglify', function(){
-  return gulp.src("dist/sticky-sidebar.js")
+gulp.task('bundle', ['concat'], function(){
+  return gulp.src(["dist/sticky-sidebar.js", "dist/jquery.sticky-sidebar.js"])
+    .pipe(sourcemaps.init())
+    // transform the files here.
+    .pipe(rollup({
+      // any option supported by Rollup can be set here.
+      entry: ['./dist/sticky-sidebar.js', './dist/jquery.sticky-sidebar.js'],
+      format: 'iife',
+      moduleName: 'stickySidebarModule'
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('uglify', ['bundle'], function(){
+  return gulp.src(["dist/sticky-sidebar.js", "dist/jquery.sticky-sidebar.js"])
     .pipe(uglify())
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
     .pipe(header(banner, {pkg}))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("dist/"));
 });
+ 
+gulp.task('default', ['babel', 'concat', 'bundle', 'uglify']);
